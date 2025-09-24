099c0c962aaca5e26c8c58de45bd59a74bbb0554
Baraa <178164194+AlbaraaAloush@users.noreply.github.com>
2025-09-24 13:30:27 +0300

    feat(ui): add hover-effect on grid divs
    Attach mouseover listener to container that applies
    the hover-effect class when hovering over .inner-div
    elements.

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
-// let container = document.querySelector(".container");
-
-// let gridArr = [];
-// let size = 10;
-// let n = size * size;
-// let divGridHeight = 100 / size;
-// let gridDivWidth = 100 / size;
-// for (let i = 0; i < n; i++) {
-//   let gridDiv = document.createElement("div");
-//   gridDiv.classList.add("inner-div");
-//   gridArr.push(gridDiv);
-//   gridDiv.setAttribute(
-//     "style",
-//     `flex: 1 1 calc(${gridDivWidth}% - 10px); height: ${divGridHeight}% `
-//   );
-// }
-
-// for (item of gridArr) {
-//   container.appendChild(item);
-// }
-
 function createGrid(size) {
-  let container = document.querySelector(".container");
-  let gridArr = [];
   let n = size * size;
   let divGridHeight = 100 / size;
   let gridDivWidth = 100 / size;
-    
+
   for (let i = 0; i < n; i++) {
     let gridDiv = document.createElement("div");
     gridDiv.classList.add("inner-div");
-    gridArr.push(gridDiv);
     gridDiv.setAttribute(
       "style",
       `flex: 1 1 calc(${gridDivWidth}% - 5px); height: ${divGridHeight}% `
@@ -37,5 +13,12 @@ function createGrid(size) {
     container.appendChild(gridDiv);
   }
 }
+
+let container = document.querySelector(".container");
+container.addEventListener("mouseover", (event) => {
+  if (event.target.matches(".inner-div")) { 
+    event.target.classList.add("hover-effect")
+  }
+});
 
 createGrid(10);
